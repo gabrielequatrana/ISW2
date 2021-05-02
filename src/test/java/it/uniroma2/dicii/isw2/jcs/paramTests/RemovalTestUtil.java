0195a81fd3854ef2d2ba3a27d6aeb004507b2735
Gabriele <gabriele.quatrana@outlook.com>
2021-05-02 21:47:46 +0200

    Refactorized tests

--- src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/RemovalTestUtil.java
+++ src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/RemovalTestUtil.java
@@ -2,13 +2,65 @@ package it.uniroma2.dicii.isw2.jcs.paramTests;
 
 import static org.junit.Assert.*;
 
+import org.apache.jcs.JCS;
 import org.junit.Test;
 
 public class RemovalTestUtil {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void runTestPutThenRemoveCategorical(int start, int end) throws Exception {
+		JCS jcs = JCS.getInstance("testCache1");
+
+		for (int i = start; i <= end; i++) {
+			jcs.put(i + ":key", "data" + i);
+		}
+
+		for (int i = end; i >= start; i--) {
+			String res = (String) jcs.get(i + ":key");
+			if (res == null) {
+				assertNotNull("[" + i + ":key] should not be null", res);
+			}
+		}
+
+		System.out.println("Confirmed that " + (end - start) + " items could be found");
+
+		for (int i = start; i <= end; i++) {
+			jcs.remove(i + ":");
+			assertNull(jcs.get(i + ":key"));
+		}
+
+		System.out.println("Confirmed that " + (end - start) + " items were removed");
+		System.out.println(jcs.getStats());
 	}
 
+	@Test
+	public void runPutInRange(int start, int end) throws Exception {
+		JCS jcs = JCS.getInstance("testCache1");
+
+		for (int i = start; i <= end; i++) {
+			jcs.put(i + ":key", "data" + i);
+		}
+
+		for (int i = end; i >= start; i--) {
+			String res = (String) jcs.get(i + ":key");
+			if (res == null) {
+				assertNotNull("[" + i + ":key] should not be null", res);
+			}
+		}
+	}
+
+	@Test
+	public void runGetInRange(int start, int end, boolean check) throws Exception {
+		JCS jcs = JCS.getInstance("testCache1");
+
+		// don't care if they are found
+		for (int i = end; i >= start; i--) {
+			String res = (String) jcs.get(i + ":key");
+			if (check && res == null) {
+				assertNotNull("[" + i + ":key] should not be null", res);
+			}
+
+		}
+
+	}
 }
--- src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/ZeroSizeCacheUnitTest.java
+++ src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/ZeroSizeCacheUnitTest.java
@@ -2,13 +2,55 @@ package it.uniroma2.dicii.isw2.jcs.paramTests;
 
 import static org.junit.Assert.*;
 
+import org.apache.jcs.JCS;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ZeroSizeCacheUnitTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/** number to get each loop */
+	private static int items = 20000;
+
+	@Before
+	public void setUp() throws Exception {
+		JCS.setConfigFilename("/TestZeroSizeCache.ccf");
+		JCS.getInstance("testCache1");
 	}
 
+	@Test
+	public void testPutGetRemove() throws Exception {
+		JCS jcs = JCS.getInstance("testCache1");
+
+		for (int i = 0; i <= items; i++) {
+			jcs.put(i + ":key", "data" + i);
+		}
+
+		// all the gets should be null
+		for (int i = items; i >= 0; i--) {
+			String res = (String) jcs.get(i + ":key");
+			if (res == null) {
+				assertNull("[" + i + ":key] should be null", res);
+			}
+		}
+
+		// test removal, should be no exceptions
+		jcs.remove("300:key");
+
+		// allow the shrinker to run
+		Thread.sleep(500);
+
+		// do it again.
+		for (int i = 0; i <= items; i++) {
+			jcs.put(i + ":key", "data" + i);
+		}
+
+		for (int i = items; i >= 0; i--) {
+			String res = (String) jcs.get(i + ":key");
+			if (res == null) {
+				assertNull("[" + i + ":key] should be null", res);
+			}
+		}
+
+		System.out.println(jcs.getStats());
+	}
 }
