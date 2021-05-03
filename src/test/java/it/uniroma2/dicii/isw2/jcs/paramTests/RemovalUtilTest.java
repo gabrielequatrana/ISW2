644374bd6f40ed980ab255719f28824967e03a52
Gabriele <gabriele.quatrana@outlook.com>
2021-05-03 18:15:27 +0200

    Implemented tests

---
+++ src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/RemovalUtilTest.java
@@ -0,0 +1,125 @@
+package it.uniroma2.dicii.isw2.jcs.paramTests;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+
+import org.apache.jcs.JCS;
+import org.apache.jcs.engine.control.CompositeCacheManager;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(value=Parameterized.class)
+public class RemovalUtilTest {
+	
+	private int start;
+	private int end;
+	private boolean check;
+	
+	private JCS jcs;
+	private static CompositeCacheManager ccm;
+	
+	@Parameters
+	public static Collection<Object[]> getTestParameters() {
+		return Arrays.asList(new Object[][] {
+			{1,2,false},
+			{2,10,false},
+			{20,50,false},	// VEDERE TRUE-FALSE che fanno
+			{10,15,false},
+		});
+	}
+	
+	public RemovalUtilTest(int start, int end, boolean check) {
+		this.start = start;
+		this.end = end;
+		this.check = check;
+	}
+	
+	@BeforeClass
+	public static void configure() {
+		RemovalUtilTest.ccm = CompositeCacheManager.getUnconfiguredInstance();
+		Properties props = new Properties();
+		
+		props.put("jcs.region.testCache1", "");
+		props.put("jcs.region.testCache1.cacheattributes", "org.apache.jcs.engine.CompositeCacheAttributes");
+		props.put("jcs.region.testCache1.cacheattributes.MaxObjects", "1000");
+		props.put("jcs.region.testCache1.cacheattributes.MemoryCacheName", "org.apache.jcs.engine.memory.lru.LRUMemoryCache");
+		
+		ccm.configure(props);
+	}
+	
+	@AfterClass
+	public static void release() {
+		RemovalUtilTest.ccm.release();
+	}
+	
+	@Before
+	public void configureEnvironment() throws Exception {
+		this.jcs = JCS.getInstance("testCache1");
+	}
+	
+	@After
+	public void releaseResources() throws Exception {
+		this.jcs.clear();
+	}
+
+	@Test
+	public void runTestPutThenRemoveCategorical() throws Exception {
+		
+		for (int i = start; i <= end; i++) {
+			jcs.put(i + ":key", "data" + i);
+		}
+
+		for (int i = end; i >= start; i--) {
+			String res = (String) jcs.get(i + ":key");
+			if (res == null) {
+				assertNotNull("[" + i + ":key] should not be null", res);
+			}
+		}
+
+		System.out.println("Confirmed that " + (end - start) + " items could be found");
+
+		for (int i = start; i <= end; i++) {
+			jcs.remove(i + ":");
+			assertNull(jcs.get(i + ":key"));
+		}
+
+		System.out.println("Confirmed that " + (end - start) + " items were removed");
+		System.out.println(jcs.getStats());
+	}
+
+	@Test
+	public void runPutInRange() throws Exception {
+
+		for (int i = start; i <= end; i++) {
+			jcs.put(i + ":key", "data" + i);
+		}
+
+		for (int i = end; i >= start; i--) {
+			String res = (String) jcs.get(i + ":key");
+			if (res == null) {
+				assertNotNull("[" + i + ":key] should not be null", res);
+			}
+		}
+	}
+
+	@Test
+	public void runGetInRange() throws Exception {
+
+		// don't care if they are found
+		for (int i = end; i >= start; i--) {
+			String res = (String) jcs.get(i + ":key");
+			if (check && res == null) {
+				assertNotNull("[" + i + ":key] should not be null", res);
+			}
+		}
+	}
+}
--- src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/ZeroSizeCacheUnitTest.java
+++ src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/ZeroSizeCacheUnitTest.java
@@ -2,24 +2,76 @@ package it.uniroma2.dicii.isw2.jcs.paramTests;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+
 import org.apache.jcs.JCS;
+import org.apache.jcs.engine.control.CompositeCacheManager;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(value=Parameterized.class)
 public class ZeroSizeCacheUnitTest {
 
 	/** number to get each loop */
-	private static int items = 20000;
+	private int items;
+	
+	private JCS jcs;
+	private static CompositeCacheManager ccm;
+	
+	@Parameters
+	public static Collection<Integer[]> getTestParameters() {
+		return Arrays.asList(new Integer[][] {
+			{2000},
+			{1000},
+			{1},
+		});
+	}
+	
+	public ZeroSizeCacheUnitTest(int items) {
+		this.items = items;
+	}
 
 	@Before
 	public void setUp() throws Exception {
-		JCS.setConfigFilename("/TestZeroSizeCache.ccf");
+		ZeroSizeCacheUnitTest.ccm = CompositeCacheManager.getUnconfiguredInstance();
+		Properties props = new Properties();
+		
+		// VEDERE COME USERE ccf file pd
+		props.put("jcs.default", "");
+		props.put("jcs.default.cacheattributes", "org.apache.jcs.engine.CompositeCacheAttributes");
+		props.put("jcs.default.cacheattributes.MaxObjects", "0");
+		props.put("jcs.default.cacheattributes.MemoryCacheName", "org.apache.jcs.engine.memory.lru.LRUMemoryCache");
+		props.put("jcs.default.cacheattributes.UseMemoryShrinker", "true");
+		props.put("jcs.default.cacheattributes.MaxMemoryIdleTimeSeconds", "3600");
+		props.put("jcs.default.cacheattributes.ShrinkerIntervalSeconds", "1");
+		props.put("jcs.default.elementattributes", "org.apache.jcs.engine.ElementAttributes");
+		props.put("jcs.default.elementattributes.IsEternal", "false");
+		props.put("jcs.default.elementattributes.MaxLifeSeconds", "600");
+		props.put("jcs.default.elementattributes.IdleTime", "1800");
+		props.put("jcs.default.elementattributes.IsSpool", "true");
+		props.put("jcs.default.elementattributes.IsRemote", "true");
+		props.put("jcs.default.elementattributes.IsLateral", "true");
+		
+		ccm.configure(props);
+		
+		//JCS.setConfigFilename("/TestZeroSizeCache.ccf");
 		JCS.getInstance("testCache1");
+	}
+	
+	@After
+	public void cleanUp() throws Exception {
+		this.jcs.clear();
 	}
 
 	@Test
 	public void testPutGetRemove() throws Exception {
-		JCS jcs = JCS.getInstance("testCache1");
+		this.jcs = JCS.getInstance("testCache1");
 
 		for (int i = 0; i <= items; i++) {
 			jcs.put(i + ":key", "data" + i);
