68340346c7463e9291618fbed7d9a481f4d6990f
Gabriele <gabriele.quatrana@outlook.com>
2021-05-31 00:19:25 +0200

    Test

--- src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/RemovalUtilTest.java
+++ src/test/java/it/uniroma2/dicii/isw2/jcs/paramTests/RemovalUtilTest.java
@@ -15,7 +15,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public class RemovalTestUtil {
+public class RemovalUtilTest {
 	
 	private int start;
 	private int end;
@@ -38,7 +38,7 @@ public class RemovalTestUtil {
 		});
 	}
 	
-	public RemovalTestUtil(int start, int end, boolean check) {
+	public RemovalUtilTest(int start, int end, boolean check) {
 		this.start = start;
 		this.end = end;
 		this.check = check;
