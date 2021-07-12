f76e713096c603d908569861a81846c63c8f3fcc
Gabriele <gabriele.quatrana@outlook.com>
2021-07-12 19:59:17 +0200

    Test

--- jacoco.sh
+++ jacoco.sh
@@ -3,4 +3,4 @@ PATH_JCS_SRC="./lib/jcs-1.3"
 PATH_JCS_JAR="./lib"
 
 mkdir -p target/jacoco-gen/jcs-coverage/
-java -jar ${PATH_JACOCO_CLI_JAR}/jacococli.jar report target/jacoco.exec --classfiles ${PATH_JCS_JAR}/jcs-1.3.jar --sourcefiles ${PATH_JCS_SRC} --html target/jacoco-gen/jcs-coverage/ --xml target/jacoco-gen/jcs-coverage/file.xml --csv target/jacoco-gen/jcs-coverage/file.csv
+java -jar ${PATH_JACOCO_CLI_JAR}/jacococli.jar report target/jacoco.exec --classfiles ${PATH_JCS_JAR}/jcs-1.3.jar --sourcefiles ${PATH_JCS_SRC} --html target/jacoco-gen/jcs-coverage/ --xml target/jacoco-gen/jcs-coverage/jacoco.xml --csv target/jacoco-gen/jcs-coverage/jacoco.csv
